55ee20123056652aeb5f657ad5a9437bde5b82c9
Calos <359000081@qq.com>
2018-10-01 15:25:55 +0800

    using java.net socket to listen to a server, and get input output streams

---
+++ Demo.java
@@ -0,0 +1,26 @@
+//the course tutorial can be found @
+// https://www.runoob.com/java/java-networking.html
+// assume that I want to use java to get a webpage, and read the content to echo.
+//1. import thing is , a class must be put in the same file with filename same with the class
+//this one is to listen to a system port, and receive outputstream from the port, then transfer the outputstream to dataoutputstream. use socket to connect to a remote server port, use client getiputstream to get server response. 
+import java.net.*;
+import java.io.*;
+
+public class Demo{
+    public static void main(String[] args) {
+        // String serverName=args[0];
+        // int port=Integer.parseInt(args[1]);
+        String serverName="localhost";
+        int port=90;
+        try {
+            Socket client=new Socket(serverName,port);
+
+
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        System.out.println("Yes");
+
+    }
+}
