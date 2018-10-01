4fc7e6293360700e8f1f5ca4a62986e9af8b58c9
Calos <359000081@qq.com>
2018-10-01 15:39:57 +0800

    add plan for server thread implementation

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@
 // assume that I want to use java to get a webpage, and read the content to echo.
 //1. import thing is , a class must be put in the same file with filename same with the class
 //this one is to listen to a system port, and receive outputstream from the port, then transfer the outputstream to dataoutputstream. use socket to connect to a remote server port, use client getiputstream to get server response. 
-//the concept in this part is , socket bind remote server port, client getoutputstream, then input streams to current host
+//the concept in this part is , socket bind remote server port, client getoutputstream, then input streams to current host,this one is just a socket client, I will write another socket server that implements thread to accept connections of such client.
 import java.net.*;
 import java.io.*;
 
