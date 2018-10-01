7635dfc64e1b39cad73aadb4d27f9d1ee2d24490
Calos <359000081@qq.com>
2018-10-01 15:29:36 +0800

    add explanation of the concept

--- Demo.java
+++ Demo.java
@@ -3,6 +3,7 @@
 // assume that I want to use java to get a webpage, and read the content to echo.
 //1. import thing is , a class must be put in the same file with filename same with the class
 //this one is to listen to a system port, and receive outputstream from the port, then transfer the outputstream to dataoutputstream. use socket to connect to a remote server port, use client getiputstream to get server response. 
+//the concept in this part is , socket bind remote server port, client getoutputstream, then input streams to current host
 import java.net.*;
 import java.io.*;
 
