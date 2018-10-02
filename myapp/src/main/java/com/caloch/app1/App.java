5baf0574ae235b97196f5ecda0ae9a9e01a9ee60
Calos <359000081@qq.com>
2018-10-02 18:14:27 +0800

    add myapp maven app

---
+++ myapp/src/main/java/com/caloch/app1/App.java
@@ -0,0 +1,16 @@
+package com.caloch.app1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Helper h1=new Helper();
+        h1.printN();
+        Helper.printName();
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ myapp/src/main/java/com/caloch/app1/Helper.java
@@ -0,0 +1,12 @@
+package com.caloch.app1;
+
+public class Helper{
+
+    public static void printName() {
+        System.out.println("name is Caloch");
+    }
+
+    public void printN() {
+        System.out.println("name is printN");
+    }
+}
